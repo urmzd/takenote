bc343275946fafb47f707d74ed25169ba7978042
Urmzd <urmzds@gmail.com>
2022-01-14 20:04:23 -0500

    refactor - change folder structure

---
+++ src/init/mod.rs
@@ -0,0 +1,14 @@
+struct Message {
+    message: String,
+    date: String,
+    // By default, this should be the computer's name.
+    author: String,
+}
+
+struct File {
+    filename: String,
+    dateCreated: String,
+    dateLastUpdated: String,
+}
+
+fn createMarkdown() {}
