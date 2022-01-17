34c0ee5a9869130a283d42e81015b723e0f0e406
Urmzd <urmzds@gmail.com>
2022-01-16 21:21:02 -0500

    impl - function to handle environment variables

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,21 +1,51 @@
 pub mod init {
+    use std::env::{self, VarError};
+
+    use serde::{Deserialize, Serialize};
 
     #[allow(dead_code)]
-    struct DirConfig {
-        parent: String,
-        children: Vec<String>,
+    struct Environment {
+        default_dir: String,
+    }
+
+    impl Environment {
+        fn new(default_dir: String) -> Environment {
+            Environment {
+                default_dir: default_dir,
+            }
+        }
     }
 
     #[allow(dead_code)]
+    #[derive(Serialize, Deserialize, Debug)]
     struct Config {
-        default_project: String,
+        children: Option<Vec<String>>,
+    }
+
+    #[allow(dead_core)]
+    fn parse_environment_variables() -> Result<Environment, VarError> {
+        let default_dir = env::var("TAKENOTE_DEFAULT_DIR")?;
+
+        let environment = Environment::new(default_dir);
+        Ok(environment)
+    }
+
+    impl Config {
+        /*
+         *        #[allow(dead_code)]
+         *        fn read_config_from_file(file_content: &String) -> Result<Config, Error> {
+         *            return Result<Config, _> = toml::from_str(file_content)
+         *        }
+         *
+         *        #[allow(dead_code)]
+         *        fn read_config_from_cli(args: &[String]) -> Result<Config, &str> {}
+         */
+
+        /*
+         *fn merge_config() {}
+         */
     }
 
     #[allow(dead_code)]
-    fn read_config() {
-        let _random = DirConfig {
-            parent: String::from("RANDOM"),
-            children: Vec::new(),
-        };
-    }
+    fn run() {}
 }
