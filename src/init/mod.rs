f2e1e7ac40370bd63b8e2aeefb6cda44335bc289
Urmzd <urmzds@gmail.com>
2022-01-24 22:54:31 -0500

    impl stubs for config retrieval

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,49 +1,61 @@
 pub mod init {
     use std::env::{self, VarError};
 
-    use serde::{Deserialize, Serialize};
+    use serde::{Deserialize, Serialize, de::Error};
 
     #[allow(dead_code)]
     struct Environment {
         default_dir: String,
     }
 
+    /// TODO - Retrieve and deserialize environment file.
+    ///
+    ///
+    ///
+    ///
+    ///
     impl Environment {
-        fn new(default_dir: String) -> Environment {
-            Environment {
-                default_dir: default_dir,
-            }
+        #[allow(dead_code)]
+        fn pull() -> Result<Environment, VarError> {
+            let default_dir = env::var("TAKENOTE_DEFAULT_DIR")?;
+
+            let enviroment = Environment { default_dir };
+            return Ok(enviroment);
         }
     }
 
     #[allow(dead_code)]
     #[derive(Serialize, Deserialize, Debug)]
-    struct Config {
-        children: Option<Vec<String>>,
+    struct ServiceProviders {
+        linkedin: String,
+        medium: String,
     }
 
-    #[allow(dead_core)]
-    fn parse_environment_variables() -> Result<Environment, VarError> {
-        let default_dir = env::var("TAKENOTE_DEFAULT_DIR")?;
-
-        let environment = Environment::new(default_dir);
-        Ok(environment)
+    struct Config {
+        children: Option<Vec<String>>,
+        providers: Option<ServiceProviders>,
     }
 
     impl Config {
-        /*
-         *        #[allow(dead_code)]
-         *        fn read_config_from_file(file_content: &String) -> Result<Config, Error> {
-         *            return Result<Config, _> = toml::from_str(file_content)
-         *        }
-         *
-         *        #[allow(dead_code)]
-         *        fn read_config_from_cli(args: &[String]) -> Result<Config, &str> {}
-         */
+        #[allow(dead_code)]
+        fn read_config_from_file(file_content: &String) -> Result<Config, Error> {
+            let config: Result<Config, Error> = toml::from_str(file_content)
+            return Ok(Config {
+                children: None,
+                providers: None,
+            });
+        }
 
-        /*
-         *fn merge_config() {}
-         */
+        #[allow(dead_code)]
+        fn read_config_from_cli(args: &[String]) -> Result<Config, &str> {
+            return Ok(Config {
+                children: None,
+                providers: None,
+            });
+        }
+
+        #[allow(dead_code)]
+        fn merge_config() {}
     }
 
     #[allow(dead_code)]
