603a7b729bb01481667fd20294d8808a41856839
Urmzd <urmzds@gmail.com>
2022-02-06 18:41:15 -0500

    test manually that functions are working

--- src/init.rs
+++ src/init.rs
@@ -25,24 +25,17 @@ struct ServiceProviders {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Config {
+pub struct Config {
     name: String,
     children: Option<Vec<String>>,
     providers: Option<ServiceProviders>,
 }
 
 impl Config {
-    #[allow(dead_code)]
-    fn read_config_from_file(file_path: &str) -> Result<Config, Box<dyn Error>> {
+    pub fn read_config_from_file(file_path: &String) -> Result<Config, Box<dyn Error>> {
         let contents = fs::read_to_string(file_path)?;
         let config = toml::from_str(&contents)?;
 
         return Ok(config);
     }
 }
-
-#[allow(dead_code)]
-fn init_folders() {}
-
-#[allow(dead_code)]
-fn run() {}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::env::VarError;
+use std::error::Error;
 
 mod init;
 
-fn main() -> Result<(), VarError> {
+fn main() -> Result<(), Box<dyn Error>> {
     let env = init::Environment::pull()?;
     println!("GOT MY VARIABLE YUH {}", env.default_dir);
+    let config = init::Config::read_config_from_file(&env.default_dir)?;
+    let ser = toml::to_string(&config)?;
+    println!("GOT MY CONFIGS YUH {}", ser);
 
     Ok(())
 }
