86671175ed6ac758b7d9cf6e23e76db79129d2b1
Urmzd <urmzds@gmail.com>
2022-02-06 18:15:00 -0500

    refactor mod name

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,50 +1,48 @@
-pub mod init {
-    use std::{
-        env::{self, VarError},
-        error::Error,
-        fs,
-    };
+use std::{
+    env::{self, VarError},
+    error::Error,
+    fs,
+};
 
-    use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
-    pub struct Environment {
-        pub default_dir: String,
-    }
-
-    impl Environment {
-        pub fn pull() -> Result<Environment, VarError> {
-            let default_dir = env::var("TAKENOTE_DEFAULT_HEAD")?;
-
-            return Ok(Environment { default_dir });
-        }
-    }
-
-    #[derive(Serialize, Deserialize, Debug)]
-    struct ServiceProviders {
-        linkedin: String,
-        medium: String,
-    }
-
-    #[derive(Serialize, Deserialize, Debug)]
-    struct Config {
-        name: String,
-        children: Option<Vec<String>>,
-        providers: Option<ServiceProviders>,
-    }
-
-    impl Config {
-        #[allow(dead_code)]
-        fn read_config_from_file(file_path: &str) -> Result<Config, Box<dyn Error>> {
-            let contents = fs::read_to_string(file_path)?;
-            let config = toml::from_str(&contents)?;
-
-            return Ok(config);
-        }
-    }
-
-    #[allow(dead_code)]
-    fn init_folders() {}
-
-    #[allow(dead_code)]
-    fn run() {}
+pub struct Environment {
+    pub default_dir: String,
 }
+
+impl Environment {
+    pub fn pull() -> Result<Environment, VarError> {
+        let default_dir = env::var("TAKENOTE_DEFAULT_HEAD")?;
+
+        return Ok(Environment { default_dir });
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ServiceProviders {
+    linkedin: String,
+    medium: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    name: String,
+    children: Option<Vec<String>>,
+    providers: Option<ServiceProviders>,
+}
+
+impl Config {
+    #[allow(dead_code)]
+    fn read_config_from_file(file_path: &str) -> Result<Config, Box<dyn Error>> {
+        let contents = fs::read_to_string(file_path)?;
+        let config = toml::from_str(&contents)?;
+
+        return Ok(config);
+    }
+}
+
+#[allow(dead_code)]
+fn init_folders() {}
+
+#[allow(dead_code)]
+fn run() {}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env::VarError;
 mod init;
 
 fn main() -> Result<(), VarError> {
-    let env = init::init::Environment::pull()?;
+    let env = init::Environment::pull()?;
     println!("GOT MY VARIABLE YUH {}", env.default_dir);
 
     Ok(())
