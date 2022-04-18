94c03a93032bdf5270753d42f9ac797ebfad85e1
Urmzd <urmzds@gmail.com>
2022-04-18 18:22:18 -0400

    Remove old tests.

--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,7 @@
 use std::{
     env,
     error::Error,
-    fs, os,
+    fs,
     path::{Path, PathBuf},
 };
 
@@ -10,6 +10,7 @@ use serde::{Deserialize, Serialize};
 const DEFAULT_HEAD_ENV_VAR: &str = "TAKENOTE_DEFAULT_HEAD";
 
 /// Holds the values of the environment variables required to run `takenote`.
+#[derive(Debug, Serialize)]
 pub struct Environment {
     /// Points to the directory which contains the entrypoint.
     pub default_dir: PathBuf,
@@ -44,8 +45,8 @@ pub struct Config {
     children: Option<Vec<String>>,
 }
 
-impl From<&Path> for Config {
-    fn from(file_path: &Path) -> Self {
+impl From<PathBuf> for Config {
+    fn from(file_path: PathBuf) -> Self {
         let contents = fs::read_to_string(file_path).unwrap();
         let config: Config = toml::from_str(&contents).unwrap();
         config
@@ -64,82 +65,5 @@ impl Config {
         std::fs::create_dir(working_dir)?;
 
         return Ok(());
-    }
-}
-
-///
-/// # TODO:
-///     - [ ]
-///
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::{error::Error, io::Write};
-    use tempfile::NamedTempFile;
-
-    #[test]
-    fn given_valid_file_path_when_file_is_read_then_config_is_provided(
-    ) -> Result<(), Box<dyn Error>> {
-        // Arrange
-        let tmp_config = Config {
-            name: "Urmzd Mukhammadnaim".to_string(),
-            children: None,
-        };
-
-        let tmp_config_string = toml::to_string(&tmp_config)?;
-        let mut tmp_file = NamedTempFile::new()?;
-
-        tmp_file.write_all(&tmp_config_string.as_bytes())?;
-
-        // Act
-        let config_to_match = match tmp_file.path().to_str() {
-            Some(value) => Config::read_config_from_file(&value.to_string()),
-            None => Err("THIS SHOULD NEVER HAPPEN".into()),
-        }?;
-
-        // Assert
-        assert_eq!(config_to_match, tmp_config);
-
-        Ok(())
-    }
-
-    #[test]
-    fn given_env_var_is_set_when_environment_is_read_then_environment_struct_is_provided(
-    ) -> Result<(), Box<dyn Error>> {
-        // Arrange.
-        let current_env_var = env::var(DEFAULT_HEAD_ENV_VAR);
-        let test_var_value = "TEST_VAR_VALUE";
-
-        env::set_var(DEFAULT_HEAD_ENV_VAR, &test_var_value);
-
-        // Act.
-        let enviroment = Environment::pull()?;
-
-        // Assert.
-        assert_eq!(enviroment.default_dir, test_var_value.to_string());
-
-        // Cleanup.
-        if current_env_var.is_ok() {
-            env::set_var(DEFAULT_HEAD_ENV_VAR, current_env_var.unwrap());
-        }
-
-        Ok(())
-    }
-
-    #[test]
-    #[should_panic]
-    fn given_unset_env_var_when_environment_is_read_errr_is_thrown() {
-        // Arrange
-        let current_env_var = env::var(DEFAULT_HEAD_ENV_VAR);
-
-        env::remove_var(DEFAULT_HEAD_ENV_VAR);
-
-        let enviroment = Environment::pull();
-
-        if current_env_var.is_ok() {
-            env::set_var(DEFAULT_HEAD_ENV_VAR, current_env_var.unwrap());
-        }
-
-        enviroment.unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::error::Error;
 
+use takenote::init;
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let env = init::Environment::new();
+    println!("{:?}", env);
+    let config = init::Config::from(env.default_dir);
+    println!("{:?}", config);
     Ok(())
 }
